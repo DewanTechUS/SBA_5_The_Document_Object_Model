7a2055de22db8fc4baeecd3d084cf0e32af495db
Dewan Mahmud Rocky <rockyit10@gmail.com>
2025-11-07 12:19:09 -0500

    initialize SBA blog project

---
+++ notes.js
@@ -0,0 +1,4 @@
+// this is a note for my classmates who use my codes but dont mention me
+// please give me credits if you are using my codes
+// thank you
+//---------------------------------------------------------------------
