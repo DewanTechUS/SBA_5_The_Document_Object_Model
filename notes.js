d06a1662ae9ed33d99ac35f0385bc179950c7125
Dewan Mahmud Rocky <rockyit10@gmail.com>
2025-11-07 13:51:05 -0500

    feat(Notes.js)Add resources

--- notes.js
+++ notes.js
@@ -1,4 +1,11 @@
+// Dewan Mahmud Rocky
 // this is a note for my classmates who use my codes but dont mention me
-// please give me credits if you are using my codes
-// thank you
+// please give me credits if you are using my codes or acknowledge me in your project
+// i spent a lot of time and effort to write these codes and i would appreciate it
+// thank you.
+// how to commit properly: git commit -m "your message" --author="Dewan Mahmud Rocky
+// follow this for commit like developer---- https://gist.github.com/joshbuchea/6f47e86d2510bce28f8e7f42ae84c716
+// https://www.freecodecamp.org/news/git-cherry-pick-avoid-duplicate-commits/
+
 //---------------------------------------------------------------------
+// ref https://ps-lms.vercel.app/curriculum/se/412/sba#:~:text=Create%20New%20Posts,if%20validation%20fails.
